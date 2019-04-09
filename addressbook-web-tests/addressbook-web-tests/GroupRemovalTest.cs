3460997bb1336ca71cac741e4ff8f31726abc8c9
maxim-iliano-s <maxim_iliano_s@mail.ru>
2019-04-09 11:59:39 +0300

    Лекция 2.1 добавил ТестБасе класс

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+   
+    [Test]
+    public void GroupRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin" , "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupPage();
+            //driver.FindElement(By.LinkText("Logout")).Click();
+        }
+    }
+}
